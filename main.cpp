7323bfc998cbea7fce2ccaefbb859a5ed3da684b
junios-study <junios@naver.com>
2026-04-01 10:18:37 +0900

    SDL2 라이브러리 설치 연결

--- main.cpp
+++ main.cpp
@@ -2,18 +2,34 @@
 #include "Engine.h"
 #include "World.h"
 
+#include "SDL.h"
+//사용할 라이브러리 복사
+//include 경로 추가
+//lib 경로 추가
+//library 파일 등록 
+
+//사용할 라이브러리 파일 추가
+#pragma comment(lib, "SDL2")
+#pragma comment(lib, "SDL2main")
+
+
 using namespace std;
 
 
-int main()
+int SDL_main(int argc, char* argv[])
 {
-	GEngine->GetWorld()->Load("level01.umap");
-
-	GEngine->Run();
-
-	delete GEngine;
-
 	return 0;
 }
 
+//int main()
+//{
+//	GEngine->GetWorld()->Load("level01.umap");
+//
+//	GEngine->Run();
+//
+//	delete GEngine;
+//
+//	return 0;
+//}
+//
 
